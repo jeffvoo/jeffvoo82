b2dfd20130e261aa521297b160201efac7700e6d
jeffvoo <101925177+jeffvoo@users.noreply.github.com>
2022-04-02 15:10:28 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from textblob import TextBlob
+
+from flask import Flask
+
+app = Flask(__name__)
+
+from flask import render_template,request
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        text = request.form.get("text")
+        print(text)
+        s = TextBlob(text).sentiment
+        return(render_template("index.html", result=s))
+    else:
+        return(render_template("index.html", result="2"))
+
+if __name__=="__main__":
+    app.run()
+
+
+# In[ ]:
+
+
+
+
